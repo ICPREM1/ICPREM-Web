03f7a372c90fef01bf70da4c6b412a8181f68cf4
Akpa Onyedikachukwu <akpa3220@gmail.com>
2024-03-18 11:29:26 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/js/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -11,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/plugin.cjs")],
 };
