3f9638741c47aa48666433760c05856e5a435662
Akpa Onyedikachukwu <akpa3220@gmail.com>
2024-03-20 11:18:49 +0100

    Committing changes before pulling

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import "@rainbow-me/rainbowkit/styles.css";
-// import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-// import { WagmiProvider } from "wagmi";
-// import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
-// import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
